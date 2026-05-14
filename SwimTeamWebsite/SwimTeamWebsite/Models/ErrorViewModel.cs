f3cff709ee4159784b40e70209e881c165329555
Dylan <kevinnmissaski@gmail.com>
2026-05-14 10:49:50 +0100

    again

--- SwimTeamWebsite/SwimTeamWebsite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SwimTeamWebsite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
